964e6dda1958b4ca924da9b5fe01b880ce6ecbb4
Manjunath Kantigavi <mkantigavi@gmail.com>
2025-01-21 12:31:34 +0530

    Removed slash

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ app.use(express.urlencoded({ extended: true }));
 
 // CORS configuration
 app.use(cors({
-    origin: ['http://127.0.0.1:5500', 'http://localhost:5500', 'http://127.0.0.1:5501', 'http://localhost:5501','https://devdynamic25.github.io/ThinkDesignSolutions/'],
+    origin: ['http://127.0.0.1:5500', 'http://localhost:5500', 'http://127.0.0.1:5501', 'http://localhost:5501','https://devdynamic25.github.io/ThinkDesignSolutions'],
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true
